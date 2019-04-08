cce99372de8dc18d84c2b2562b4fe5dab6c72029
bwmarcos <bw.marcos@gmail.com>
2019-04-07 22:49:05 -0300

    add select operation where operation

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -13,29 +13,38 @@ public class Programa {
 	
 	public static void main(String[] args) {
 		
-		//Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
-		//Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
-		//Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
+		/*Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
+		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
+		Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");*/
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
 		
-		/*em.getTransaction().begin();
+	/*	em.getTransaction().begin();
 		em.persist(p1);
 		em.persist(p2);
 		em.persist(p3);
 		em.getTransaction().commit();
 		em.close();*/
 		
-		TypedQuery<Pessoa> query =
-			      em.createQuery("SELECT p FROM Pessoa p", Pessoa.class);
+	/*	TypedQuery<Pessoa> query =
+			      em.createQuery("SELECT p FROM Pessoa p ", Pessoa.class);
 			  List<Pessoa> results = query.getResultList();
 		
+			  */
+			  TypedQuery<Pessoa> query =
+				      em.createQuery("SELECT p FROM Pessoa p WHERE p.nome = :nome", Pessoa.class);
+			query.setParameter("nome", "Joaquim Torres");
+				  List<Pessoa> results = query.getResultList(); 
 		
-		//Pessoa p = em.find(Pessoa.class, 2);
+	//	Pessoa p = em.find(Pessoa.class, 2);
+		
+		
 		
 		//System.out.println(p);
-		System.out.println(results);
+	System.out.println(results);
+		
+		
 		
 		
 	}
